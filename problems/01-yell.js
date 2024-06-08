05241f6f310ef65ce83b8bb92994bf0826da0bf0
#KhaineShrout <Khaine.s@icloud.com>
2024-06-08 16:49:48 -0400

    Problem 1

--- problems/01-yell.js
+++ problems/01-yell.js
@@ -4,9 +4,11 @@ version of that string.
 */
 
 // Your code here 
-
+function yell(string){
+    return string.toUpperCase()+"!!!"
+}
 // console.log(yell("I want to go to the store")); // "I WANT TO GO TO THE STORE!!!"
-// console.log(yell("Time to program")); // "TIME TO PROGRAM!!!"
+//console.log(yell("Time to program")); // "TIME TO PROGRAM!!!"
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = yell;
